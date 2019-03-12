341cca01b22edd046683f4b4a6677fb264c25e46
Tomáš Hetmer <TomHetmer@users.noreply.github.com>
2019-03-12 01:42:44 +0100

    Update server.py

--- server.py
+++ server.py
@@ -10,16 +10,19 @@ import cgi
 import os
 import hashlib
 import hmac
+import sys
+
+sys.stdout = sys.stderr
 
 def verify_token(token, timestamp, signature):
-    hmac_digest = hmac.new(key=os.environ['MAILGUN_API_KEY'],
-                           msg='{}{}'.format(timestamp, token),
+    hmac_digest = hmac.new(key=os.environ['MAILGUN_API_KEY'].encode('utf-8'),
+                           msg='{}{}'.format(timestamp, token).encode('utf-8'),
                            digestmod=hashlib.sha256).hexdigest()
-    return hmac.compare_digest(unicode(signature), unicode(hmac_digest))
+    return hmac.compare_digest(signature.encode('utf-8'), hmac_digest.encode('utf-8'))
 
 class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
+    def _set_headers(self, code=200):
+        self.send_response(code)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
@@ -27,7 +30,7 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
 
     def do_POST(self):
-        self._set_headers()
+        print("new message")
 
         form = cgi.FieldStorage(
             fp=self.rfile,
@@ -37,12 +40,24 @@ class S(BaseHTTPRequestHandler):
                      })
 
         if verify_token(form.getvalue('token'), form.getvalue('timestamp'), form.getvalue('signature')):
+            print("token ok")
             message = form.getvalue('body-mime')
-            cmd = "echo bundle exec rails r 'Channel::Driver::MailStdin.new(trusted: true)'"
-            pipe = Popen(cmd, shell=True, cwd=os.environ['ZAMMAD_DIR'], stdin=PIPE).stdin
+            cmd = "bundle exec rails r 'Channel::Driver::MailStdin.new(trusted: true)'"
+            process = Popen(cmd, shell=True, cwd=os.environ['ZAMMAD_DIR'], stdin=PIPE)
+            pipe = process.stdin
             pipe.write(message.encode('utf-8'))
             pipe.close()
-            return
+            process.wait()
+
+            if process.returncode == 0:
+                print("processed")
+                self._set_headers(200)
+            else:
+                print("not processed")
+                self._set_headers(500)
+        else:
+            print("token not ok")
+            self._set_headers(403)
 
 def run(server_class=HTTPServer, handler_class=S, port=1337):
     server_address = ('', port)
